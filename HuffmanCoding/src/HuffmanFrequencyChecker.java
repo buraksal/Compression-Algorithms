53275dc07cde20b451c42936de4bcb1ee2feb664
buraksal <buraksal1996@gmail.com>
2021-03-22 00:06:15 +0300

    Implemented a class that finds the frequencies of each char in the given string.

---
+++ HuffmanCoding/src/HuffmanFrequencyChecker.java
@@ -0,0 +1,56 @@
+public class HuffmanFrequencyChecker {
+	
+	private int[] tmpTable = new int[255];
+	public int[] freqTable;
+	public char[] dataTable;
+	
+	//Default Constructor
+	public HuffmanFrequencyChecker() {
+		
+	}
+	
+	public void checkFrequency(String str) {
+		int asciiVal;
+		for(int i = 0; i < str.length(); i++) {
+			asciiVal = str.charAt(i);
+			tmpTable[asciiVal]++;
+		}
+		this.cleanZeroes();
+	}
+	
+	private int findUnique() {
+		int cnt = 0;
+		for(int i = 0; i < tmpTable.length; i++) {
+			if(tmpTable[i] != 0) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+	
+	private void cleanZeroes() {
+		int tmp = 0;
+		int cnt = this.findUnique();
+		freqTable = new int[cnt];
+		dataTable = new char[cnt];
+		for(int i = 0; i < tmpTable.length; i++) {
+			if(tmpTable[i] != 0) {
+				freqTable[tmp] = tmpTable[i];
+				dataTable[tmp] = (char)i;
+				tmp++;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		String str = "Let's see the simple code to convert char to int in java.";
+		HuffmanFrequencyChecker freqTable = new HuffmanFrequencyChecker();
+		freqTable.checkFrequency(str);
+		int[] table = freqTable.freqTable;
+		char[] dataTable = freqTable.dataTable;
+		//FrequencyChecker freqChecker = new FrequencyChecker();
+		for(int i = 0; i < table.length; i++) {
+			System.out.println("Char: " + dataTable[i] + " with frequency: " +table[i]);
+		}
+	}
+}
